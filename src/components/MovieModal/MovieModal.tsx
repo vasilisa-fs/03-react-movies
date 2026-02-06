398b6b38934775fc69feb37f881a91393aea9764
Vasilisa-fs <vasilisa.bychkova.fs@gmail.com>
2026-02-06 20:25:38 +0200

    Add poster

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -46,9 +46,9 @@ export default function MovieModal({ movie, onClose }: MovieModalProps) {
         </button>
         <img
           src={
-            movie.poster_path
-              ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-              : "/no-poster.png"
+            movie.backdrop_path
+              ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
+              : "/no-poster-modal.png"
           }
           alt={movie.title}
           className={css.image}
